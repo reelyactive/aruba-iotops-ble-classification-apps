2e6117c38769eebcc88da80ffbab41d1ad5457b7
jeffyactive <jeff@reelyactive.com>
2025-03-03 14:37:10 -0500

    Implemented Eddystone-UID in addition to iBeacon

--- lua/interoperable-identifier.lua
+++ lua/interoperable-identifier.lua
@@ -14,10 +14,14 @@ function decode(address, addressType, advType, elements)
 
   -- Eddystone-UID constants
   SERVICE_DATA_TYPE = 0x16
-  SERVICE_UUID = 0xfeaa
+  EDDYSTONE_SERVICE_UUID = 0xfeaa
+  EDDYSTONE_UID_FRAME_TYPE = 0x00
+  SIGNAL_LOSS_AT_1M_DBM = 41
 
   -- See https://reelyactive.github.io/interoperable-identifier/
-  IBEACON_UUID_PREFIX = "\x49\x6f\x49\x44\x43\x4f\x44\x45\xb7\x3e"
+  IOID_UUID_PREFIX = "\x49\x6f\x49\x44\x43\x4f\x44\x45\xb7\x3e"
+  IOID_NAMESPACE_PREFIX = "\x49\x6f\x49\x44"
+  local elidedEntityUuid, instanceId, instanceIdHex, txPower
   
   local manufacturerData = elements[MANUFACTURER_DATA_TYPE]
   local serviceData = elements[SERVICE_DATA_TYPE]
@@ -35,75 +39,94 @@ function decode(address, addressType, advType, elements)
     -- Confirm Apple iBeacon frame with InteroperaBLE Identifier UUID prefix
     if companyCode ~= APPLE_COMPANY_CODE or
        frameType ~= IBEACON_FRAME_TYPE or
-       iBeaconUuidPrefix ~= IBEACON_UUID_PREFIX then
+       iBeaconUuidPrefix ~= IOID_UUID_PREFIX then
       return handle
     end
 
     -- Determine the iBeacon properties
-    local iBeaconUuid = string.sub(manufacturerData, 5, 20)
-    local iBeaconMajor = string.unpack(">I2",
-                                       string.sub(manufacturerData, 21, 22))
-    local iBeaconMinor = string.unpack(">I2",
-                                       string.sub(manufacturerData, 23, 24))
-    local iBeaconTxPower = string.unpack(">i1",
-                                         string.sub(manufacturerData, 25, 25))
+    local uuid = string.sub(manufacturerData, 5, 20)
+    local major = string.unpack(">I2", string.sub(manufacturerData, 21, 22))
+    local minor = string.unpack(">I2", string.sub(manufacturerData, 23, 24))
+    local txPower1m = string.unpack(">i1", string.sub(manufacturerData, 25, 25))
 
     -- Determine the InteroperaBLE Identifier properties
-    local instanceId = ((iBeaconMajor & 0xfff) << 16) + iBeaconMinor
-    local instanceIdHex = string.format("%07x", instanceId)
-    local entityUuidSuffix = string.sub(manufacturerData, 15, 20)
-
-    -- Determine the scoped deviceId based on address type
-    local scopedDeviceId = address
-    if ble_mac_address_is_private(addressType) then
-      scopedDeviceId = instanceIdHex
-    end
-
-    -- Set common attribute for InteroperaBLE Identifier    
-    handle:setExtend("txPower", iBeaconTxPower)
-    --handle:setCalibratedPower(iBeaconTxPower) -- TODO: use when supported
-
-    -- DirAct (uses DirAct device class)
-    if entityUuidSuffix == "\x44\x69\x72\x41\x63\x74" then
-      handle:setDeviceClass("DirAct")
-      handle:setDeviceClassScopedDeviceId("DirAct", instanceIdHex)
-      return handle
-    end
-
-    -- Local .mp3 file
-    if entityUuidSuffix == "\x2e\x2f\x2e\x6d\x70\x33" then
-      handle:setExtend("uri", "file:/" .. instanceIdHex .. ".mp3")
-    end
-
-    -- Unicode Code Point
-    if entityUuidSuffix == "\x55\x54\x46\x2d\x33\x32" then
-      handle:setExtend("unicodeCodePoint", utf8.char(instanceId))
-    end
-
-    -- Motion detection
-    if entityUuidSuffix == "\x4d\x6f\x74\x69\x6f\x63" then
-      handle:setMotion(true)
-      handle:setExtend("isMotionDetected", true)
-    end
-
-    -- Button pressed
-    if entityUuidSuffix == "\x42\x75\x74\x74\x6f\x6e" then
-      handle:setExtend("isButtonPressed", true)
-    end
-
-    -- Set the device class and scoped deviceId
-    handle:setDeviceClass("iBeacon") -- TODO: change class when available
-    handle:setDeviceClassScopedDeviceId("iBeacon", scopedDeviceId)
-    return handle
+    elidedEntityUuid = string.sub(uuid, 1, 4) .. string.sub(uuid, 11, 16)
+    instanceId = ((major & 0xfff) << 16) + minor
+    instanceIdHex = string.format("%07x", instanceId)
+    txPower = txPower1m + SIGNAL_LOSS_AT_1M_DBM
 
   end
 
   -- Attempt to decode Eddystone-UID
   ----------------------------------
-  if serviceData ~= nil and #serviceData >= 25 then
-    -- TODO
+  if serviceData ~= nil and #serviceData == 22 then
+
+    -- Determine the company code, frame type and proximity UUID prefix
+    local serviceUuid = string.unpack("<I2", (string.sub(serviceData, 1, 2)))
+    local frameType = string.unpack(">i1", (string.sub(serviceData, 3, 3)))
+    local namespacePrefix = string.sub(serviceData, 5, 8)
+
+    -- Confirm Eddystone-UID frame with InteroperaBLE Identifier prefix
+    if serviceUuid ~= EDDYSTONE_SERVICE_UUID or
+       frameType ~= EDDYSTONE_UID_FRAME_TYPE or
+       namespacePrefix ~= IOID_NAMESPACE_PREFIX then
+      return handle
+    end
+
+    -- Determine the Eddystone-UID properties
+    local rangingData = string.unpack(">i1", string.sub(serviceData, 4, 4))
+    local namespace = string.sub(serviceData, 5, 14)
+    local instance = string.sub(serviceData, 15, 20)
+
+    -- Determine the InteroperaBLE Identifier properties
+    elidedEntityUuid = namespace
+    instanceId = string.unpack(">I6", instance) & 0xfffffff
+    instanceIdHex = string.format("%07x", instanceId)
+    txPower = rangingData
+
   end
 
-  -- No matching iBeacon or Eddystone-UID payload found
+  -- Set common attribute for InteroperaBLE Identifier
+  handle:setExtend("txPower", txPower)   
+  --handle:setCalibratedPower(txPower - SIGNAL_LOSS_AT_1M_DBM) -- TODO: use?
+
+  -- DirAct (uses DirAct device class)
+  if elidedEntityUuid == "\x49\x6f\x49\x44\x44\x69\x72\x41\x63\x74" then
+    handle:setDeviceClass("DirAct")
+    handle:setDeviceClassScopedDeviceId("DirAct", instanceIdHex)
+    return handle
+  end
+
+  -- Local .mp3 file
+  if elidedEntityUuid == "\x49\x6f\x49\x44\x2e\x2f\x2e\x6d\x70\x33" then
+    handle:setExtend("uri", "file:/" .. instanceIdHex .. ".mp3")
+  end
+
+  -- Unicode Code Point
+  if elidedEntityUuid == "\x49\x6f\x49\x44\x55\x54\x46\x2d\x33\x32" then
+    handle:setExtend("unicodeCodePoint", utf8.char(instanceId))
+  end
+
+  -- Motion detection
+  if elidedEntityUuid == "\x49\x6f\x49\x44\x4d\x6f\x74\x69\x6f\x63" then
+    handle:setMotion(true)
+    handle:setExtend("isMotionDetected", true)
+  end
+
+  -- Button pressed
+  if elidedEntityUuid == "\x49\x6f\x49\x44\x42\x75\x74\x74\x6f\x6e" then
+    handle:setExtend("isButtonPressed", true)
+  end
+
+  -- Determine the scoped deviceId based on address type
+  local scopedDeviceId = address
+  if ble_mac_address_is_private(addressType) then
+    scopedDeviceId = instanceIdHex
+  end
+
+  -- Set the device class and scoped deviceId
+  handle:setDeviceClass("iBeacon") -- TODO: change class when available
+  handle:setDeviceClassScopedDeviceId("iBeacon", scopedDeviceId)
   return handle
+
 end
